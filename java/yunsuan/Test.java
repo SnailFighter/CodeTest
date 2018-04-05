d05a7bd77f162e7214d3c3298d1c0314f74db13d
SnailFighter <cht_feng@163.com>
2018-04-05 12:13:27 +0800

    运算测试

--- java/yunsuan/Test.java
+++ java/yunsuan/Test.java
@@ -1,5 +1,9 @@
+import java.lang.Thread;
+import java.lang.Exception;
+
+
 class Test {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws Exception {
 		int i = 16;
 		System.out.println("i>>>1="+(i>>>1));
 		i = 16;
@@ -28,6 +32,9 @@ class Test {
 		System.out.println("i>>2="+(i>>2));
 		i = -8;
 		System.out.println("i/4="+(i/4));
+
+
+		Thread.sleep(10000000);
 	}
 }
 
