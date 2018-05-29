6c5504446702bfc03855a44ae8cfcf75e3ef59a5
SnailFighter <cht_feng@163.com>
2018-05-29 17:07:59 +0800

    类中方法装饰器

--- python/base_grammer/object/obj.py
+++ python/base_grammer/object/obj.py
@@ -1,4 +1,4 @@
-# java 中有构造器，python的构造器是__init__()这样的方法
+#java 中有构造器，python的构造器是__init__()这样的方法
 # python只能有一个__init__ 这样的方法，不像java可以有多个构造方法，不过在python中在初始化方法中写多个参数，指定默认值的方式实现构造器的多态。
 # 类中的方法第一个参数都有个指向自己的变量，默认写self，当然也可以写做其他名字
 
@@ -14,6 +14,20 @@ class Obj(object):
 		self.name=name
 
 
+
+	#@classmethod 修饰的方法相当于java中static修饰的方法，是类的方法。
+	@classmethod
+	def getAgeAndName(self):
+		#这么写是要包错误的：type object 'Obj' has no attribute 'name' 
+		# 因为name是实例化后才有的 
+		#return self.name
+		print("yttyty")
+	#@property 相当与直接返回了方法的返回值
+	@property
+	def getPro(self):
+		return self.name
+
+
 if __name__ == '__main__':
 	obj = Obj('xxxx',20)
 	print(obj.getName())
@@ -21,5 +35,9 @@ if __name__ == '__main__':
 	print(obj.getName())
 	#访问属性
 	print(obj.name)
-	print(obj.__shortName)
-
+	# obj.__shortName报错
+	#print(obj.__shortName)
+	#注意，这里直接使用的类名.方法名,另外print打印为：None，因为getAgeAndName()方法返回值为空
+	print(Obj.getAgeAndName())
+	#注意，这里方法名后名没有使用括号
+	print(obj.getPro)
