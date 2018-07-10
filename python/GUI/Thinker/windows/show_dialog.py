b09f16060e8025ac2830726d63f6b283cf87ac3e
SnailFighter <cht_feng@163.com>
2018-07-10 22:09:59 +0800

    布局和网上一个参考例子
    布局和网上一个参考例子

--- python/GUI/Thinker/windows/show_dialog.py
+++ python/GUI/Thinker/windows/show_dialog.py
@@ -8,4 +8,22 @@ def welcome():
     window.title('Snail Mail')
     window.geometry('1000x650')
 
+    # 创建frame
+    frm_left = tk.Frame(window,height= 650,width=200,bg='white')
+    frm_left.pack(side='left')
+    # 创建frame
+    frm_right = tk.Frame(window,height=650,width=795);
+    frm_right.pack(side='right')
+
+    #
+    frm_right_top = tk.Frame(frm_right, height=60, width=795,bg='white')
+    frm_right_top.pack(side='top')
+
+    frm_right_line = tk.Frame(frm_right, height=5, width=795,)
+    frm_right_line.pack(side='top')
+
+    frm_right_bottom = tk.Frame(frm_right, height=585, width=795, bg='white')
+    frm_right_bottom.pack(side='bottom')
+
+
     window.mainloop()
