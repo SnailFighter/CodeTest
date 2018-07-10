e50cd75115086855d630da576ca494b2afe9f17e
SnailFighter <cht_feng@163.com>
2018-07-10 21:28:45 +0800

    修改弹框内容
    修改弹框内容

--- python/GUI/Thinker/windows/show_dialog.py
+++ python/GUI/Thinker/windows/show_dialog.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.messagebox
 
 def welcome():
-    tk.messagebox.showinfo(title="welcome",message="热烈欢迎")
+    tk.messagebox.showinfo(title="welcome",message="欢迎登陆Snail Mail!")
     # 开启一个新的窗口
     window = tk.Tk()
     window.title('Snail Mail')
