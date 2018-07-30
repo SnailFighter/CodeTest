e4a73ed2b8468ff284449d50e69c594ba87a65eb
SnailFighter <cht_feng@163.com>
2018-07-30 14:54:17 +0800

    python 数据库连接
    数据库连接，添加日志类

---
+++ python/GUI/Thinker/LogScan/Log.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 日志详细信息
+
+
+class Log(object):
+    def _int_(self, pack_code, origin=1):
+        pass
--- python/GUI/Thinker/LogScan/mysql_conn.py
+++ python/GUI/Thinker/LogScan/mysql_conn.py
@@ -1,10 +1,11 @@
 import pymysql.cursors
+import json
 
 # Connect to the database
-connection = pymysql.connect(host='47.107.23.206',
+connection = pymysql.connect(host='147.107.123.216',
                              port=3308,
                              user='root',
-                             password='opmysql',
+                             password='mysql',
                              db='resources',
                              charset='utf8',
                              cursorclass=pymysql.cursors.DictCursor)
@@ -14,7 +15,12 @@ try:
         # Read a single record
         sql = "select * from check_package_detail_log limit 30"
         cursor.execute(sql)
+        # 返回的是list，list里面是dict数据
         result = cursor.fetchmany(10)
+        for x in result:
+            print(x['id'])
         print(result)
+
+
 finally:
     connection.close()
